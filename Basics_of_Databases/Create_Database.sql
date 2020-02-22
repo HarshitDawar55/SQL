cdeea386c2d08e9f671f1b1fff75aeef7efbdb94
HarshitDawar55 <harshitr.dawar55@gmail.com>
2020-02-23 00:00:31 +0530

    Creating a Database!

---
+++ Basics_of_Databases/Create_Database.sql
@@ -0,0 +1,5 @@
+-- command for creating a database; 
+-- test is the database name;
+-- use name of the database which you want to create instead of test which is mentioned in the command;
+
+create database test;
