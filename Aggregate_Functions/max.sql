47c1f53f97e7113ae1afbede71e2eb511280fcfa
HarshitDawar55 <harshitr.dawar55@gmail.com>
2020-02-24 22:35:54 +0530

    max funtion in SQL Explanation!

---
+++ Aggregate_Functions/max.sql
@@ -0,0 +1,8 @@
+-- max function is used to find the maximum for a particular column of a table;
+-- I have used test table name for example;
+-- 'price' passed in max represents name of the column, it will find the maximum value from the price column;
+
+-- syntax is:
+
+
+select max(price) from test;
