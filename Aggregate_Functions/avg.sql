755b67a2061d90fb2d8a013c7f10f6f889049a66
HarshitDawar55 <harshitr.dawar55@gmail.com>
2020-02-24 22:39:18 +0530

    average funtion in SQL Explanation!

---
+++ Aggregate_Functions/avg.sql
@@ -0,0 +1,8 @@
+-- avg function is used to find the average of all the values for a particular column of a table;
+-- I have used test table name for example;
+-- 'price' passed in max represents name of the column, it will find the average of all the values from the price column;
+
+-- syntax is:
+
+
+select avg(price) from test;
