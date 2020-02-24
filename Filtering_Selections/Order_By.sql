568904a95f9f5799aabbf49ec20ea487265d8716
HarshitDawar55 <harshitr.dawar55@gmail.com>
2020-02-24 22:46:04 +0530

    AExplaning order by function to filter final putput!

---
+++ Filtering_Selections/Order_By.sql
@@ -0,0 +1,9 @@
+-- order by function is used to order the final output based on the values of a particular column of a table, either ascending or descending;
+-- I have used test table name for example;
+-- * is used to select everything from the table;
+-- "order by price", means it will filter the output and display all the outputs based on the values in the price column.
+-- Automatically it will filter in ascending order.
+-- syntax is:
+
+
+select * from test order by price;
