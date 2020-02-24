4ee289e8499473cc9a2ec3bc826fc2f0814e1cfb
HarshitDawar55 <harshitr.dawar55@gmail.com>
2020-02-24 22:28:49 +0530

    count funtion in SQL Explanation!

---
+++ Aggregate_Functions/count.sql
@@ -0,0 +1,8 @@
+-- count function is used to count the rows in a particular table;
+-- I have used test table name for example;
+-- '*' passed in count represents everything, if we want to count a particluar column we can add the column name instead of that star. 
+
+-- syntax is:
+
+
+select count(*) from test;
