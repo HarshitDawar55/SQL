fa63e7b2adc6de4977723416d8e8f313c53a61fe
HarshitDawar55 <harshitr.dawar55@gmail.com>
2020-02-24 22:37:35 +0530

    sum funtion in SQL Explanation!

---
+++ Aggregate_Functions/sum.sql
@@ -0,0 +1,8 @@
+-- sum function is used to find the sum for a particular column of a table;
+-- I have used test table name for example;
+-- 'price' passed in max represents name of the column, it will find the sum of all the value from the price column;
+
+-- syntax is:
+
+
+select sum(price) from test;
