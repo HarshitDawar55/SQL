d490d2504bd71ee862c75f8b5f85ed74871ee156
HarshitDawar55 <harshitr.dawar55@gmail.com>
2020-02-24 22:34:44 +0530

    min funtion in SQL Explanation!

---
+++ Aggregate_Functions/min.sql
@@ -0,0 +1,8 @@
+-- min function is used to find the minimum for a particular column of a table;
+-- I have used test table name for example;
+-- 'price' passed in min represents name of the column, it will find the minimum value from the price column;
+
+-- syntax is:
+
+
+select min(price) from test;
