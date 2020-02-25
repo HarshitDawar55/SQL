011a6aa5ac7b6f1bd976978df3e85fd635b3860e
HarshitDawar55 <harshitr.dawar55@gmail.com>
2020-02-25 12:39:02 +0530

    using a Database!

---
+++ Basics_of_Databases/Use_Database.sql
@@ -0,0 +1,5 @@
+-- command for using a database; 
+-- test is the database name;
+-- use name of the database which you want to use instead of test which is mentioned in the command;
+
+use database test;
