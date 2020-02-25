3302d45493f33364fb40fccdaeb796269f34e711
HarshitDawar55 <harshitr.dawar55@gmail.com>
2020-02-25 13:08:25 +0530

    Creating a table in sql!

---
+++ Basics_of_Databases/creating_table.sql
@@ -0,0 +1,8 @@
+-- command for creating a table; 
+-- practice is the database name;
+-- use name of the table which you want to use create instead of praactice which is mentioned in the command;
+-- col1 is the placeholder for the first column name;
+-- dt1 is the placeholder for the datatype of 1st column;
+-- '...' is for other adding columns, it is not the part of the actual command.
+
+create table practice(col1 dt1, col2 dt2, ...);
