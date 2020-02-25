8aedce9ebbdb730f94ff573bf6494308aec51f29
HarshitDawar55 <harshitr.dawar55@gmail.com>
2020-02-25 12:38:50 +0530

    Dropping a Database!

--- Basics_of_Databases/Drop_database.sql
+++ Basics_of_Databases/Drop_database.sql
@@ -1,5 +1,5 @@
 -- command for dropping a database; 
 -- test is the database name;
--- use name of the database which you want to create instead of test which is mentioned in the command;
+-- use name of the database which you want to drop instead of test which is mentioned in the command;
 
 drop database test;
